59ce0ca82993e21704eb2757e86d3b0a485f45ae
noonquiver <camilo.hernandez25@hotmail.com>
2022-09-14 18:11:22 -0500

    Drawing a checkerboard and a circle.

--- Core Graphics/ViewController.swift
+++ Core Graphics/ViewController.swift
@@ -26,6 +26,8 @@ class ViewController: UIViewController {
         
         switch currentDrawType {
         case 0: drawRectangle()
+        case 1: drawCircle()
+        case 2: drawCheckerboard()
         default: break
         }
     }
@@ -33,13 +35,45 @@ class ViewController: UIViewController {
     func drawRectangle() {
         let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
         let image = renderer.image { context in
-            let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512)
+            let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512).insetBy(dx: 20, dy: 20)
             
             context.cgContext.setFillColor(UIColor.red.cgColor)
             context.cgContext.setStrokeColor(UIColor.black.cgColor)
             context.cgContext.setLineWidth(10) // 5 points outside and 5 points inside.
             context.cgContext.addRect(rectangle)
             context.cgContext.drawPath(using: .fillStroke)
+        }
+        
+        imageView.image = image
+    }
+    
+    func drawCircle() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        let image = renderer.image { context in
+            let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512).insetBy(dx: 20, dy: 20)
+            
+            context.cgContext.setFillColor(UIColor.red.cgColor)
+            context.cgContext.setStrokeColor(UIColor.black.cgColor)
+            context.cgContext.setLineWidth(10) // 5 points outside and 5 points inside.
+            context.cgContext.addEllipse(in: rectangle)
+            context.cgContext.drawPath(using: .fillStroke)
+        }
+        
+        imageView.image = image
+    }
+    
+    func drawCheckerboard() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 611, height: 485))
+        let image = renderer.image { context in
+            context.cgContext.setFillColor(UIColor.black.cgColor)
+            
+            for row in 0 ..< 8 {
+                for column in 0 ..< 10 {
+                    if (row + column).isMultiple(of: 2) {
+                        context.cgContext.fill(CGRect(x: column * 64, y: row * 64, width: 64, height: 64))
+                    }
+                }
+            }
         }
         
         imageView.image = image
