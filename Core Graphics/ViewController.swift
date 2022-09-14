5e488089ebef47cd41cbb7a4312e5a9b5a117203
noonquiver <camilo.hernandez25@hotmail.com>
2022-09-14 17:46:32 -0500

    First commit.

---
+++ Core Graphics/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Core Graphics
+//
+//  Created by Camilo Hernández Guerrero on 14/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    var currentDrawType = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        drawRectangle()
+    }
+    
+    @IBAction func redrawTapped(_ sender: Any) {
+        currentDrawType += 1
+        
+        if currentDrawType > 5 {
+            currentDrawType = 0
+        }
+        
+        switch currentDrawType {
+        case 0: drawRectangle()
+        default: break
+        }
+    }
+    
+    func drawRectangle() {
+        
+    }
+}
